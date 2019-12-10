b3de1a3158fe6c88c5716dec0c3a1378d81305ca
John <gerakl00@gmail.com>
2019-12-10 13:29:32 +0300

    NO COMMENT

--- main.c
+++ main.c
@@ -56,4 +56,95 @@ void addENVToOUTPUT() {
 }
 
 void startJob() {
-  short int state = 0; // 0 - 
+  short int state = 0;
+  char inChar;
+
+  while (!feof(globalArgs.inputFile)){
+    fscanf(globalArgs.inputFile,"%c",&inChar);
+    if (inChar == '$' && state != 1) {
+      state = 1;
+      continue;
+    } else if ((inChar == ' ' || inChar == '\n' || inChar == '\0') && (state == 1)) {
+      state = 2;
+    }
+    if (state == 1) {
+      addCharToENVVar(inChar);
+    } else if (state == 2) {
+      addENVToOUTPUT();
+      state = 0;
+    }
+    if (state == 0) {
+      addCharToOutput(inChar);
+    }
+  }
+}
+
+void display_usage(char* name) {
+  printf("\nUSAGE:\n%s [-h] \n\nARGS: \n-h: Help\n\n", name);
+  exit(EXIT_SUCCESS);
+}
+
+int getStartData(int argc, char** argv) {
+  int opt = 0;
+
+  opt = getopt(argc, argv, optString);
+  while (opt != -1) {
+    switch (opt) {
+      case 'h':
+        display_usage(argv[0]);
+        break;
+      default:
+        break;
+    }
+    opt = getopt(argc, argv, optString);
+  }
+
+  if (optind < argc) {
+    globalArgs.inputPath = argv[optind++];
+    if (optind < argc) {
+      globalArgs.outputPath = argv[optind];
+    }
+  }
+
+  return 1;
+}
+
+int main(int argc, char** argv) {
+
+  globalArgs.inputPath = NULL;
+  globalArgs.outputPath = NULL;
+
+  if (getStartData(argc, argv) == 0) printf("Error occured - programm has stopped\n");
+
+  if (globalArgs.outputPath != NULL) {
+    if ((globalArgs.outputFile = fopen(globalArgs.outputPath, "w")) == NULL) {
+      fprintf(stderr, "Can not open output file!\n");
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    globalArgs.outputFile = stdout;
+  }
+
+  if (globalArgs.inputPath != NULL) {
+    if ((globalArgs.inputFile = fopen(globalArgs.inputPath, "r")) == NULL) {
+      fprintf(stderr, "Can not open input file!\n");
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    globalArgs.inputFile = stdin;
+  }
+
+  startJob();
+
+  if (globalArgs.inputPath == NULL) {
+    endOfOutputString -= 2;
+  }
+
+  if (endOfOutputString != 0) {
+    for (int i = 0; i <= endOfOutputString; i++) {
+      fprintf(globalArgs.outputFile, "%c", outputString[i]);
+    }
+  }
+
+  return 0;
+}
